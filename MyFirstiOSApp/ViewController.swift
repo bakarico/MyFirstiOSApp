53d77c6bd0e485f0d8cd653922d785db128a3164
bakarico <yawaza.nico@gmail.com>
2015-02-25 23:29:59 +0800

    change the background randomly when click the btn

--- MyFirstiOSApp/ViewController.swift
+++ MyFirstiOSApp/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var helloWorldLabel: UILabel!
     @IBOutlet weak var clickMeButton: UIButton!
+    @IBOutlet weak var backgroupImageView: UIImageView!
+    @IBOutlet weak var copyrightLable: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +28,14 @@ class ViewController: UIViewController {
 
     @IBAction func clickMeButtonTapped(sender: UIButton) {
         self.helloWorldLabel.text = "I am not hungry."
+        self.helloWorldLabel.backgroundColor = UIColor(white: 1, alpha: 0.8)
+        self.copyrightLable.backgroundColor = UIColor(white: 1, alpha: 0.8)
+       
+        var randomNumberString = String(format: "girl%i", arc4random_uniform(2) + 1)
+        // var randomNumberString:String = String(format: "girl%i", arc4random_uniform(2) + 1)
+        
+        self.backgroupImageView.image = UIImage(named: randomNumberString)
+        
     }
 
 }
